81ab888586e93b407eed74aecc788a5529025c5f
4llves <jhonatab4@gmail.com>
2024-03-20 23:14:30 -0300

    feat: typing knex

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,7 +1,7 @@
+import { randomUUID } from 'crypto'
 import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
 import { knex } from '../database'
-import { randomUUID } from 'crypto'
 
 export async function transactionsRoutes(app: FastifyInstance) {
   app.post('/', async (req, reply) => {
