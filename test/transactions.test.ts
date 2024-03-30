df2bc1f64b823bace96037a22cb7d16b6c4a7790
4llves <jhonatab4@gmail.com>
2024-03-29 22:17:56 -0300

    test: categorizing tests

---
+++ test/transactions.test.ts
@@ -0,0 +1,28 @@
+import { expect, test, beforeAll, afterAll, describe } from 'vitest'
+import req from 'supertest'
+import { app } from '../src/app'
+
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready() // aguardar que o app esteja pronto com valores
+  })
+
+  afterAll(async () => {
+    await app.close() // fechar a aplicacao / remover ela da memoria
+  })
+
+  test('user consegue criar uma nova transacao', async () => {
+    const response = await req(app.server).post('/transactions').send({
+      title: 'Nova Transação',
+      amount: 2500,
+      type: 'credit',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
+
+// o test é composto por basicamente 3 variaveis sendo elas
+// o enunciado ex: "user consegue criar uma nova transacao"
+// a operação
+// validação que é o que dirá se teve sucesso
