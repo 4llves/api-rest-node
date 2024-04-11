ff5ed82547d5c66de906fd5c4c18afbc1c1e52b4
4llves <jhonatab4@gmail.com>
2024-04-11 11:22:39 -0300

    chore: production config

--- src/database.ts
+++ src/database.ts
@@ -3,10 +3,13 @@ import { Knex, knex as setupKnex } from 'knex'
 import { env } from './env'
 
 export const config: Knex.Config = {
-  client: 'sqlite',
-  connection: {
-    filename: env.DATA_BASE_URL,
-  },
+  client: env.DATA_BASE_CLIENT,
+  connection:
+    env.DATA_BASE_CLIENT === 'sqlite'
+      ? {
+          filename: env.DATA_BASE_URL,
+        }
+      : env.DATA_BASE_CLIENT,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
